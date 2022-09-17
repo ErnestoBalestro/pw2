7d824ade4b9da28b656a2cf64c7eedacb9a0f00b
ErnestoBalestro <55770772+ErnestoBalestro@users.noreply.github.com>
2022-09-16 21:03:58 -0300

    Delete jwt/jwt directory

--- jwt/jwt/bff/src/test/java/dev/ifrs/BFFIT.java
+++
@@ -1,8 +0,0 @@
-package dev.ifrs;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class BFFIT extends BFFTest {
-    // Execute the same tests but in packaged mode.
-}
