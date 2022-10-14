0d6683b424c7225d88730bb8503f51bb62b8e27a
ErnestoBalestro <55770772+ErnestoBalestro@users.noreply.github.com>
2022-10-13 23:55:00 -0300

    Add files via upload

---
+++ clean/clean/src/main/java/dev/ifrs/interfaceadapters/user/controller/UserController.java
@@ -0,0 +1,29 @@
+package dev.ifrs.interfaceadapters.user.controller;
+
+import javax.enterprise.context.RequestScoped;
+import javax.transaction.Transactional;
+
+import dev.ifrs.interfaceadapters.user.repository.Repository;
+import dev.ifrs.model.User;
+import dev.ifrs.usecase.user.UseCase;
+import dev.ifrs.usecase.user.UseCaseInterface;
+
+@RequestScoped
+@Transactional
+public class UserController implements UserControllerInterface {
+
+    private UseCaseInterface uc;
+    private Repository repo;
+    
+    public UserController(){
+        this.uc = new UseCase();
+        this.repo = new Repository();
+    }
+
+    @Override
+    public User save(String name, String password) {
+        User user = uc.save(name, password);
+        return repo.save(user);
+    }
+    
+}
---
+++ clean/clean/src/main/java/dev/ifrs/model/User.java
@@ -0,0 +1,13 @@
+package dev.ifrs.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class User {
+
+    private String name;
+
+    private String password;
+    
+}
