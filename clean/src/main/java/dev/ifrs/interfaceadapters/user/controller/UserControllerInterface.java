9a2a96db3b7baec4ea618c18a54159f865fc5b6e
ErnestoBalestro <55770772+ErnestoBalestro@users.noreply.github.com>
2022-10-13 23:53:56 -0300

    Delete clean directory

--- clean/src/main/java/dev/ifrs/interfaceadapters/user/controller/UserControllerInterface.java
+++
@@ -1,9 +0,0 @@
-package dev.ifrs.interfaceadapters.user.controller;
-
-import dev.ifrs.model.User;
-
-public interface UserControllerInterface {
-
-    public User save(String name, String password);
-    
-}
